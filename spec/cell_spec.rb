56e75e295b43efb6c9f54197c3e7444c7fa2eced
Daniel Gallagher <Daniel.Gallagher92@gmail.com>
2023-05-25 17:02:51 -0600

    Test: Cell

---
+++ spec/cell_spec.rb
@@ -0,0 +1,15 @@
+require 'rspec'
+require './lib/ship'
+require './lib/cell'
+
+describe "Cell" do 
+  describe '#initialize' do
+    it 'initializes' do 
+      cell = Cell.new("B4")
+      expect(cell).to be_a(Cell)
+      expect(cell.coordinate).to eq("B4")
+      expect(cell.ship).to be(nil)
+    end
+  end
+end
+
