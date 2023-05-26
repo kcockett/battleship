9b5616a11cf39692654bf1a3b849859c1e648c3b
Daniel Gallagher <Daniel.Gallagher92@gmail.com>
2023-05-26 16:15:09 -0600

    Test: all placement tests Board

--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -46,6 +46,12 @@ describe "Board" do
       expect(board.valid_placement?(cruiser, ["A1", "B2", "C3"])).to eq(false)
       expect(board.valid_placement?(submarine, ["C2", "D3"])).to eq(false)
     end
-    
+    it "all previous checks pass" do 
+      board = Board.new
+      cruiser = Ship.new("Cruiser", 3)
+      submarine = Ship.new("Submarine", 2)
+      expect(board.valid_placement?(submarine, ["A1", "A2"])).to eq(true)
+      expect(board.valid_placement?(cruiser, ["B1", "C1", "D1"])).to eq(true)
+    end
   end
 end
