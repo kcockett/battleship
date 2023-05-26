d999604a5de23422178379ff3993aa7c30b0d9d7
Daniel Gallagher <Daniel.Gallagher92@gmail.com>
2023-05-26 11:56:06 -0600

    Test: Board #valid_placement?

--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -22,4 +22,13 @@ describe "Board" do
       expect(board.valid_coordinate?("A22")).to eq(false)
     end
   end
+  describe "#valid_placement?" do 
+    it "can check number of coordinates is same as ship" do 
+      board = Board.new
+      cruiser = Ship.new("Cruiser", 3)
+      submarine = Ship.new("Submarine", 2)
+      expect(board.valid_placement?(cruiser, ["A1", "A2",])).to eq(false)
+      expect(board.valid_placement?(submarine, ["A2", "A3", "A4"])).to eq(false)
+    end
+  end
 end
